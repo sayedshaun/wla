58468eaacadda6a88dc1efd74305d0a07f707e59
Sayed Shaun <sayedshaun4@gmail.com>
2024-12-29 23:26:15 +0600

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,39 @@ footer {visibility: hidden;}
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
-hide_streamlit_footer = """
-<style>
-footer {visibility: hidden;}
-</style>
-"""
-st.markdown(hide_streamlit_footer, unsafe_allow_html=True)
+
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
 # Function to set a background image
